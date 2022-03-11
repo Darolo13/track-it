fee1ec4bda6cfd17ac9a713ab84f989e4b7b21a2
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-03-11 01:50:38 -0700

    Layout setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
+// material-ui
+import { Grid } from '@material-ui/core';
+import useStyles from './styles';
+
+// components
+import Details from './components/Details';
+import Main from './components/Main';
+
 const App = () => {
+
+  const classes = useStyles();
+
   return (
     <div>
-      App
+      <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh' }}>
+        <Grid item xs={12} sm={4}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Main />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details title="Expense" />
+        </Grid>
+      </Grid>
     </div>
   )
 }
