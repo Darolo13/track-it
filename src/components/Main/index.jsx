0b11e8681be4acde8ae0858a20e08f219813302d
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-03-11 02:13:02 -0700

    Create Form component

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import useStyles from './styles';
+
+// material-ui
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+
+import Form from './Form';
+
+const Main = () => {
+
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance $100
+        </Typography>
+        <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
+          {/* infoCard */}
+          Try Saying: Add income for $100 in category Salary for Monday ...
+        </Typography>
+        <Divider />
+        <Form />
+      </CardContent>
+      <CardContent className={classes.cardContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            {/* <List /> */}
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Main
