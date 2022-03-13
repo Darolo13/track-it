f17dd6c55311fa479e81455aec6a5113a2a27586
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-03-13 15:56:58 -0700

    Handle Date Format

--- src/components/Main/Form/index.jsx
+++ src/components/Main/Form/index.jsx
@@ -11,6 +11,7 @@ import {
   MenuItem } from '@material-ui/core';
 import { v4 as uuidv4 } from 'uuid';
 
+import formatDate from '../../../utils/formatDate';
 import { ExpenseTrackerContext } from '../../../context/context';
 
 
@@ -23,7 +24,7 @@ const initialState = {
   amount: '',
   category: '',
   type: 'Income',
-  date: new Date(),
+  date: formatDate(new Date()),
 }
 
 const Form = () => {
@@ -69,7 +70,7 @@ const Form = () => {
         <TextField type="number" label="Amount" fullWidth value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value})} />
       </Grid>
       <Grid item xs={6}>
-        <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: e.target.value})} />
+        <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: formatDate(e.target.value)})} />
       </Grid>
       <Button className={classes.button} variant="outlined" color="primary" fullWidth onClick={createTransaction}>Create</Button>
     </Grid>
