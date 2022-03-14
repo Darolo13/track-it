0c380ecf60003ec1677e8b5c72a89e71343d7bb7
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-03-13 17:36:19 -0700

    Functional Doughnut Charts

--- src/components/Details/index.jsx
+++ src/components/Details/index.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 
+import useTransactions from '../../useTransactions';
+
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
-import { Doughnut } from 'react-chartjs-2';
 
 import useStyles from './styles'; 
 
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+
 const Details = ({ title }) => {
   const classes = useStyles();
+  const { total, chartData } = useTransactions(title);
   return (
     <div>
       <Card className={title === "Income" ? classes.income : classes.expense}>
         <CardHeader title={title} />
         <CardContent>
-          <Typography variant="h5">$50</Typography>
-          {/* <Doughnut data="DATA" /> */}
+          <Typography variant="h5">${total}</Typography>
+          <Doughnut data={chartData} />
         </CardContent>
       </Card>
     </div>
